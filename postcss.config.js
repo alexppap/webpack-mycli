097ddbaa6e93ba3e0dae25cea18ce459a3ef5e12
alexppap <1060788599@qq.com>
2020-05-31 19:51:18 +0800

    some postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')({overrideBrowserslist: ['> 0.15% in CN']})
+    require('autoprefixer')({overrideBrowserslist: ['> 0.15% in CN']}),
+    require("postcss-import"), //@import类型转换
+    require('cssnano')
   ]
 }
